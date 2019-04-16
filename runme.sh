6c91a7c3d0fc28a273346366738ac67d801a866d
borgified <borgified@gmail.com>
2019-04-16 14:11:00 -0700

    a (#2)
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a

---
+++ runme.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+COLOR_RESET='\033[0m'
+COLOR_MAGENTA='\033[0;35m'
+COLOR_CYAN='\033[0;36m'
+MYREPO=${HOME}/workdir/${TRAVIS_REPO_SLUG}
+AUTOBRANCH=testing/mybranch${VERSION}
+GITHUB_USER=borgified
+
+rm -rf ${MYREPO}
+mkdir -p ${MYREPO}
+git clone https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${MYREPO}
+cd ${MYREPO}
+git checkout -b ${AUTOBRANCH}
+echo "test ${VERSION}" >> version
+git config user.email "borgified@gmail.com"
+git config user.name "borgified"
+git commit -a -m "auto_branch_$VERSION"
+echo "we committed"
+git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${AUTOBRANCH}
+PR_URL=$(hub pull-request --no-edit)
+echo -e "${COLOR_CYAN}ATTENTION:${COLOR_RESET} review and merge ${PR_URL} to continue..."
