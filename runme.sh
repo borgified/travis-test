36a545576fb3ab6c78b55fe58b912416093d4d20
borgified <borgified@gmail.com>
2019-04-16 18:46:44 +0000

    a

---
+++ runme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+COLOR_RESET='\033[0m'
+COLOR_MAGENTA='\033[0;35m'
+COLOR_CYAN='\033[0;36m'
+MYREPO=$HOME/$TRAVIS_REPO_SLUG
+AUTOBRANCH=testing/mybranch$VERSION
+
+
+mkdir -p $MYREPO
+git clone https://$CI_USER_TOKEN@github.com/$TRAVIS_REPO_SLUG $MYREPO
+cd $MYREPO
+git checkout -b $AUTOBRANCH
+echo "test $VERSION" >> version
+git commit -a -m "a"
+git push https://$CI_USER_TOKEN@github.com/$TRAVIS_REPO_SLUG $AUTOBRANCH
+PR_URL=$(hub pull-request --no-edit) && echo "${COLOR_CYAN}ATTENTION:${COLOR_RESET} review and merge ${PR_URL} to continue..."
