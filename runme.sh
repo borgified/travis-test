0a41a06a3bda1b3c19eb734ed0ccdbccb44534ba
borgified <borgified@gmail.com>
2019-04-16 18:56:08 +0000

    a

--- runme.sh
+++ runme.sh
@@ -13,7 +13,9 @@ git clone https://$CI_USER_TOKEN@github.com/$TRAVIS_REPO_SLUG $MYREPO
 cd $MYREPO
 git checkout -b $AUTOBRANCH
 echo "test $VERSION" >> version
-git commit -a -m "a" --author "borgified <borgified@gmail.com>"
+git config user.email "borgified@gmail.com"
+git config user.name "borgified"
+git commit -a -m "a"
 git push https://$CI_USER_TOKEN@github.com/$TRAVIS_REPO_SLUG $AUTOBRANCH
 PR_URL=$(hub pull-request --no-edit)
 echo -e "${COLOR_CYAN}ATTENTION:${COLOR_RESET} review and merge ${PR_URL} to continue..."
