a6d1de4fa476c01196288918d61fce86dbbc7e49
borgified <borgified@gmail.com>
2019-04-17 12:56:57 -0700

    Test (#18)
    * a
    
    * a
    
    * a
    
    * a
    
    * a

--- run_prep.sh
+++ run_prep.sh
@@ -13,7 +13,7 @@ AUTOBRANCH=${GITHUB_USER}/prepareRelease${VERSION}
 
 function prep_workspace {
   mkdir -p ${MYREPO}
-  git clone https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${MYREPO}
+  git clone -b ${TRAVIS_BRANCH} https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${MYREPO}
   cd ${MYREPO}
   git checkout -b ${AUTOBRANCH}
 }
@@ -27,11 +27,12 @@ function push_changes {
   git config user.name "${GITHUB_USER}"
   git commit -a -m "auto_branch_$VERSION"
   git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} ${AUTOBRANCH}
-  PR_URL=$(hub pull-request --no-edit)
+  PR_URL=$(hub pull-request --no-edit -b ${TRAVIS_BRANCH})
   echo -e "${COLOR_CYAN}ATTENTION:${COLOR_RESET} review and merge ${COLOR_CYAN}${PR_URL}${COLOR_RESET}"
   echo "then to release to cocoapods use Travis CI's Trigger build with the following payload:"
   echo -e "${COLOR_MAGENTA}env:${COLOR_RESET}"
   echo -e "${COLOR_MAGENTA}  - RELEASE=true${COLOR_RESET}"
+  echo "im on ${TRAVIS_BRANCH}"
 }
 
 function main {
